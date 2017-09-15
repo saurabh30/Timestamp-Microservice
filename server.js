feebf03c5bd53bf318ed1d13472b94ea0f4276d2
System <none>
2017-09-15 11:41:41 +0000

    Import of saurabh30/Timestamp-Microservice

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.route('/_api/package.json')
   
 app.route('/')
     .get(function(req, res) {
-		  res.send('Saurabh is awesome!!');
+		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
 // Respond not found to all the wrong routes
